cbee5f695f1f885e76f1bb1258a86c6ab0f42faf
cherkasovaa <alina.ch1202@gmail.com>
2026-04-30 00:00:26 +0300

    feat: implement film card component

---
+++ src/app/components/film-card/film-card.ts
@@ -0,0 +1,13 @@
+import { Component, input } from '@angular/core';
+import { Film } from '../../services/films/films';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-film-card',
+  imports: [RouterLink],
+  templateUrl: './film-card.html',
+  styleUrl: './film-card.scss',
+})
+export class FilmCard {
+  film = input<Film>();
+}
