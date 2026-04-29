64948a6e9299a2b99a7d2192845254ff04e82e76
cherkasovaa <alina.ch1202@gmail.com>
2026-04-30 00:04:12 +0300

    feat: implement film list component with films service

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Header } from './components/header/header';
+import { FilmsList } from './components/films-list/films-list';
 import { Footer } from './components/footer/footer';
+import { Header } from './components/header/header';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, Footer],
+  imports: [RouterOutlet, Header, Footer, FilmsList],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
--- src/app/components/films-list/films-list.ts
+++ src/app/components/films-list/films-list.ts
@@ -1,16 +1,14 @@
-import { CommonModule } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { Films } from '../../services/films/films';
+import { FilmCard } from '../film-card/film-card';
 
 @Component({
   selector: 'app-films-list',
   standalone: true,
-  imports: [CommonModule],
+  imports: [FilmCard],
   templateUrl: './films-list.html',
   styleUrl: './films-list.scss',
 })
-export class FilmsList implements OnInit {
-  constructor(readonly filmsService: Films) {}
-
-  ngOnInit(): void {}
+export class FilmsList {
+  readonly filmsService = inject(Films);
 }
